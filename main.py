4b6cf912a59b989b72f4b21b7c727adb978e7859
Janan Patel <jananpatel2002@gmail.com>
2022-12-29 14:17:32 -0500

    added functionality to flip the card continuously after click

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 from tkinter import *
 import pandas
 
+delayed_color_change = None
 BACKGROUND_COLOR = "#B1DDC6"
 ARIAL_60_BOLD = ("Ariel", 60, "bold")
 ARIAL_40_ITALIC = ("Ariel", 40, "italic")
@@ -14,15 +15,29 @@ data_dict = pandas.DataFrame.to_dict(data, orient="records")  # Turns the whole 
 
 def change_background(text):
     canvas.itemconfig(image, image=card_back)
-    canvas.itemconfig(language,text="English",fill="white")
-    canvas.itemconfig(word, text=text,fill="white")
+    canvas.itemconfig(language, text="English", fill="white")
+    canvas.itemconfig(word, text=text, fill="white")
 
 
 def generate_word():
+    global delayed_color_change
+    canvas.itemconfig(image, image=card_front)
+    canvas.itemconfig(language, text="French", fill="black")
     random_number = random.randint(0, len(data_dict))
-    canvas.itemconfig(word, text=data_dict[random_number]["French"])
+    canvas.itemconfig(word, text=data_dict[random_number]["French"], fill="black")
     delayed_color_change = window.after(3000, change_background, data_dict[random_number]["English"])
 
+
+def right_clicked():
+    window.after_cancel(delayed_color_change)
+    generate_word()
+    print("right answer")
+
+
+def wrong_clicked():
+    window.after_cancel(delayed_color_change)
+    generate_word()
+    print("wrong answer")
 
 
 # --------------------------------------- UI --------------------------------------------
@@ -45,8 +60,11 @@ language = canvas.create_text(450, 150, text="French", font=ARIAL_40_ITALIC)
 word = canvas.create_text(450, 300, text="Word", font=ARIAL_60_BOLD)
 
 # Buttons
-right_button = Button(image=right_picture, highlightthickness=0, command=generate_word)
-wrong_button = Button(image=wrong_picture, highlightthickness=0, command=generate_word)
+right_button = Button(image=right_picture, highlightthickness=0, command=right_clicked)
+wrong_button = Button(image=wrong_picture, highlightthickness=0, command=wrong_clicked)
 right_button.grid(row=1, column=0, pady=20)
 wrong_button.grid(row=1, column=1, pady=20)
+
+generate_word()
+
 window.mainloop()
